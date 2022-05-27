887dbbb393ef504f5f55769008dac21aa734dd18
Adam Skowronski <236036@student.pwr.edu.pl>
2022-05-27 15:13:28 +0200

    init

---
+++ src/main/java/AddLambdaHandler.java
@@ -0,0 +1,38 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+
+public class AddLambdaHandler implements RequestStreamHandler {
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+        JSONParser parser = new JSONParser();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONObject responseJson = new JSONObject();
+
+        try {
+            JSONObject event = (JSONObject) parser.parse(reader);
+
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("message", "New item created");
+
+            JSONObject headerJson = new JSONObject();
+            headerJson.put("x-custom-header", "my custom header value");
+
+            responseJson.put("statusCode", 200);
+            responseJson.put("headers", headerJson);
+            responseJson.put("body", responseBody.toString());
+
+        } catch (Exception pex) {
+            responseJson.put("statusCode", 400);
+            responseJson.put("exception", pex);
+        }
+
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(responseJson.toString());
+        writer.close();
+    }
+}
