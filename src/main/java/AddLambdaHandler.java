34476b8f08c49fdbb1941d57cc1dc0cfc310c8b5
Adam Skowronski <236036@student.pwr.edu.pl>
2022-05-29 21:06:55 +0200

    add dynamo table

--- src/main/java/AddLambdaHandler.java
+++ src/main/java/AddLambdaHandler.java
@@ -1,9 +1,10 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.AddLambdaRequest;
-import model.AddLambdaResponse;
+import model.LambdaRequest;
+import model.LambdaResponse;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.HttpStatusCode;
@@ -14,9 +15,10 @@ import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.*;
 import java.net.URLConnection;
+import java.util.Map;
 
 public class AddLambdaHandler implements RequestStreamHandler {
-    @Override
+
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         LambdaLogger logger = context.getLogger();
 
@@ -24,19 +26,19 @@ public class AddLambdaHandler implements RequestStreamHandler {
         logger.log(inputStream.toString());
 
         try {
-            AddLambdaRequest addLambdaRequest = mapper.readValue(inputStream, AddLambdaRequest.class);
+            LambdaRequest lambdaRequest = mapper.readValue(inputStream, LambdaRequest.class);
             logger.log("MAPPED REQUEST");
-            logger.log(addLambdaRequest.toString());
-             logger.log(addLambdaRequest.getBody().toString());
+            logger.log(lambdaRequest.toString());
+             logger.log(lambdaRequest.getBody().toString());
 
-            InputStream bodyInputStream = new ByteArrayInputStream(addLambdaRequest.getBody());
+            InputStream bodyInputStream = new ByteArrayInputStream(lambdaRequest.getBody());
             String mimeType = URLConnection.guessContentTypeFromStream(bodyInputStream);
             bodyInputStream.close();
 
             if(mimeType.toLowerCase().equals("image/jpeg") || mimeType.toLowerCase().equals("image/png"))
             {
                 logger.log("REQUEST VALID");
-                logger.log("GOT IMAGE: " + addLambdaRequest.getBody().toString());
+                logger.log("GOT IMAGE: " + lambdaRequest.getBody().toString());
 
                 S3Client s3Client = S3Client.builder().region(Region.EU_WEST_1)
                         .credentialsProvider(DefaultCredentialsProvider.create())
@@ -45,16 +47,16 @@ public class AddLambdaHandler implements RequestStreamHandler {
                 PutObjectResponse sa = s3Client.putObject(PutObjectRequest.builder()
                                 .bucket("images-s3")
                                 .key("USER1FOLDER/imgNAME1").build(),
-                        RequestBody.fromBytes(addLambdaRequest.getBody()));
+                        RequestBody.fromBytes(lambdaRequest.getBody()));
 
-                AddLambdaResponse addLambdaResponse = AddLambdaResponse.builder()
+                LambdaResponse lambdaResponse = LambdaResponse.builder()
                         .statusCode(HttpStatusCode.OK)
                         //        .headers(Map.of("ResponseHDR", "123"))
                         .body(sa.toString())
                         .build();
 
                 OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-                writer.write(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(addLambdaResponse));
+                writer.write(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(lambdaResponse));
                 writer.close();
 
             } else {
--- src/main/java/GetTextLambda.java
+++ src/main/java/GetTextLambda.java
@@ -1,40 +1,97 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.AddLambdaRequest;
-import model.AddLambdaResponse;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import model.S3Event;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.SdkBytes;
-import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.*;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.util.List;
 
-public class GetTextLambda implements RequestStreamHandler {
+public class GetTextLambda implements RequestHandler<InputStream, String> {
+
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    public String handleRequest(InputStream event, Context context) {
         LambdaLogger logger = context.getLogger();
 
-        logger.log("INVOKEN LAMBDA FROM S3 UPLOAD");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false); //TODO: FIX MAPPING AND PASSING TO KOGNITO
+        logger.log(event.toString());
 
+        S3Event s3Event = mapper.convertValue(event, S3Event.class);
+
+        logger.log("MAPPED REQUEST: " + s3Event);
+
+        S3Client s3Client = S3Client.builder().region(Region.EU_WEST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        ResponseInputStream<GetObjectResponse> image = s3Client.getObject(GetObjectRequest.builder()
+                .bucket("images-s3")
+//                .key(s3Event.stream()
+//                        .map(S3Event::getObject)
+//                        .collect(Collectors.joining()))
+                .key(s3Event.getRecords().get(0).toString())
+                .build());
+
+        RekognitionClient rekognitionClient = RekognitionClient.builder()
+                .region(Region.EU_WEST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        try {
+            String textLabels = detectTextLabels(rekognitionClient, image.readAllBytes(), context);
+            logger.log("W PALE SIE NIE MIESCI: " + textLabels);
+        } catch (IOException e) {
+            logger.log("EXCEPTION ON READ BYTES FROM S3 IMAGE" + e.getMessage());
+        }
+
+        return s3Event.toString();
+    }
+
+//    @Override
+//    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+//        LambdaLogger logger = context.getLogger();
+//
+//        logger.log("process image lambda triggered by S3 image upload");
+//
 //        ObjectMapper mapper = new ObjectMapper();
-//        //   logger.log(inputStream.toString().);
+//        logger.log("RAW INTPUT STREAM: " + inputStream.toString());
+//
+//        LambdaRequest lambdaRequest = mapper.readValue(inputStream, LambdaRequest.class);
+//        logger.log("MAPPED REQUEST: " + lambdaRequest.toString());
+//        logger.log("MAPPED REQUEST BODY: " + lambdaRequest.getBody().toString());
+//
+//        RekognitionClient rekognitionClient = RekognitionClient.builder()
+//                .region(Region.EU_WEST_1)
+//                .credentialsProvider(DefaultCredentialsProvider.create())
+//                .build();
+//
+//        logger.log("UTWORZONO REKO CLIENT \n" + rekognitionClient.toString());
+//
+//        String textOut = detectTextLabels(rekognitionClient, lambdaRequest.getBody(), context);
+//        logger.log("PARSED TEXT !!!!!!XDD: " + textOut);
 //
 //        try {
-//            AddLambdaRequest addLambdaRequest = mapper.readValue(inputStream, AddLambdaRequest.class);
-//            logger.log(addLambdaRequest.toString());
+//            LambdaRequest lambdaRequest = mapper.readValue(inputStream, LambdaRequest.class);
+//            logger.log(lambdaRequest.toString());
 //            // logger.log(addLambdaRequest.getBody());
 //            logger.log("TUTAJ PRZED IFEM");
 //
-//            if(addLambdaRequest.getBody() != null)
+//            if(lambdaRequest.getBody() != null)
 //            {
 //                logger.log("W IFIE, BODY NOT NULL");
 //                logger.log("JESTESMY W IF TERAZ REQUEST DO REKONA");
@@ -50,16 +107,16 @@ public class GetTextLambda implements RequestStreamHandler {
 //
 //               // logger.log("Po wywolaniu reko: \n" + textOut);
 //
-//                AddLambdaResponse addLambdaResponse = AddLambdaResponse.builder()
+//                LambdaResponse lambdaResponse = LambdaResponse.builder()
 //                        .statusCode(HttpStatusCode.OK)
 //                        //        .headers(Map.of("ResponseHDR", "123"))
 //                 //       .body(textOut)
 //                        .build();
 //
-//                logger.log("I RESPONSE \n" + addLambdaResponse.toString());
+//                logger.log("I RESPONSE \n" + lambdaResponse.toString());
 //
 //                OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-//                writer.write(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(addLambdaResponse));
+//                writer.write(mapper.writer().withDefaultPrettyPrinter().writeValueAsString(lambdaResponse));
 //                writer.close();
 //
 //                //         responseBody.put("image", "PARSED SOME LINES !!!" + textOut);
@@ -93,25 +150,23 @@ public class GetTextLambda implements RequestStreamHandler {
 ////        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
 ////        writer.write(responseJson.toString());
 ////        writer.close();
+//
+ //   }
 
-    }
 
-
-    public static String detectTextLabels(RekognitionClient rekClient, String imageString, Context context) {
+    public static String detectTextLabels(RekognitionClient rekClient, byte[] imageBytes, Context context) {
 
         try {
             LambdaLogger logger = context.getLogger();
             logger.log("JEST TRY POCZATEK FUNC");
 
             //  InputStream sourceStream = new FileInputStream(base64image);
-            SdkBytes sourceBytes = SdkBytes.fromString(imageString, Charset.defaultCharset());
+            SdkBytes sourceBytes = SdkBytes.fromByteArray(imageBytes);
 //                    .split("\\.")[1].
 //                    replace('-', '+').replace('_', '/')));
 
             logger.log("W FUNKCJII !!! SDKBYtse:" + sourceBytes.asString(Charset.defaultCharset()));
             //   logger.log(sourceBytes.asString(Charset.defaultCharset()));
-
-
 
             // Create an Image object for the source image
             Image souImage = Image.builder()
